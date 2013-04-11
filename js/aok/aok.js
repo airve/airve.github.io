0c419324da194c2ee9c0a1ef49720cdfcf54c04e
Ryan Van Etten <ryanve@gmail.com>
2013-04-11 08:11:44 -0400

    Update to aok 1.0.0.

--- js/aok/aok.js
+++ js/aok/aok.js
@@ -1,9 +1,9 @@
 /*!
- * aok          Test suite API
+ * aok          Extensible test suite API
  * @link        github.com/ryanve/aok
  * @license     MIT
  * @copyright   2013 Ryan Van Etten
- * @version     0.8.1
+ * @version     1.0.0
  */
 
 /*jslint browser: true, devel: true, node: true, passfail: false, bitwise: true
@@ -11,34 +11,30 @@
 , nomen: true, plusplus: true, regexp: true, undef: true, sloppy: true, stupid: true
 , sub: true, white: true, indent: 4, maxerr: 100 */
 
-(function(root, name, definition) {// github.com/umdjs/umd
-    if (typeof module != 'undefined' && module['exports']) {
-        module['exports'] = definition(); // common|node|ender
-    } else { root[name] = definition(); }
+(function(root, name, make) {
+    typeof module != 'undefined' && module['exports'] ? module['exports'] = make() : root[name] = make();
 }(this, 'aok', function() {
 
     var win = window
       , doc = document
-      , console = win.console
-      , alert = win.alert
-      , uid = 1;
+      , uid = 0;
       
     /**
      * @constructor 
-     * @param  {Object=}  data
+     * @param  {*=}  data
      */
     function Aok(data) {
         var k;
-        if (null != data) {
-            for (k in data) {
-                this[k] = data[k]; 
-            }
-            this['run']();
+        if (typeof data != 'object' || !data) {
+            this['test'] = data;
+        } else for (k in data) {
+            this[k] = data[k]; 
         }
+        this['init']();
     }
 
     /**
-     * @param  {Object=}  data
+     * @param  {*=}  data
      * @return {Aok}
      */
     function aok(data) { 
@@ -52,22 +48,81 @@
     aok.prototype['pass'] = 'Pass';
     aok.prototype['fail'] = 'Fail';
     
-    // run the test and trigger the handler
+    // console methods:
+    (function(target, console, win) {
+        /**
+         * @param  {string}            name
+         * @param  {(boolean|number)=} force
+         * @param  {string=}           key
+         */
+        function assign(name, force, key) {
+            var method = console && typeof console[name] == 'function' ? function() {
+                console[name].apply(console, arguments);
+            } : function() {
+                method['force'] && win['alert'](name + ': ' + [].join.call(arguments, ' '));
+            };
+            method['force'] = !!force;
+            target[key || name] = method;
+        }
+        
+        assign('info',  1);
+        assign('warn',  1);
+        assign('error', 1);
+        assign('trace');
+        assign('log');
+        assign('log', 0, 'express');
+    }(aok, win.console, win));
+    
+    // sync the "express" method
+    aok.prototype['express'] = aok['express'];
+    
+    /**
+     * @param    {*} o  is an Object or mixed value
+     * @param    {(string|number)=}  k  
+     * @example  result(0)               // 0
+     * @example  result([1], 0)          // 1
+     */
+    function result(o, k) {
+        return 2 == arguments.length ? result.call(o, o[k]) : typeof o == 'function' ? o.call(this) : o;
+    }
+    aok['result'] = result;
+    
+    /**
+     * @return {Aok}
+     */
+    aok.prototype['init'] = function() {
+        if (this === win) { throw new Error('@this'); }
+        null == this['id'] && (this['id'] = ++uid);
+        return this['run']();
+    };
+    
+    /**
+     * @return {Aok}
+     */
     aok.prototype['run'] = function() {
-        if (!(this instanceof aok)) { throw new TypeError; }
-        null == this['id'] && (this['id'] = uid++);
-        this['test'] = !!(typeof this['test'] == 'function' ? this['test']() : this['test']);
-        this['handler'] && this['handler']();
+        if (this === win) { throw new Error('@this'); }
+        this['test'] = !!result(this, 'test'); // run the test 
+        return this['handler'](); // trigger the handler
+    };
+    
+    /**
+     * @param  {(string|number)=}    key
+     */
+    aok.prototype['cull'] = function(key) {
+        return this[this[null == key ? 'test' : key] ? 'pass' : 'fail'];
     };
 
-    // default handler can be overridden
+    /**
+     * default handler can be overridden
+     * @return {Aok}
+     */
     aok.prototype['handler'] = function() {
-        var msg = this[this['test'] ? 'pass' : 'fail'];
+        var msg = this['cull']();
         if (typeof msg == 'string') {
-            aok['log']('#' + this['id'] + ': ' + msg);
-        } else if (typeof msg == 'function') { 
-            msg.call(this); 
-        }
+            this['express']('#' + this['id'] + ': ' + msg);
+        } else if (typeof msg == 'function') {
+            msg.call(this);
+        } return this;
     };
     
     /**
@@ -77,21 +132,6 @@
     aok['id'] = function(n) {
         return doc.getElementById(n) || false;
     };
-   
-    // console methods:
-    (function(make) {
-        make('log');
-        make('trace');
-        make('info' , 1);
-        make('warn' , 1);
-        make('error', 1);
-    }(function(name, force) {
-        aok[name] = console && typeof console[name] == 'function' ? function() {
-            console[name].apply(console, arguments); 
-        } : force ? function() {
-            alert(name + ': ' + [].join.call(arguments, ' ')); 
-        } : function() {};
-    }));
     
     return aok;
 }));

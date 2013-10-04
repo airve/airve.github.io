220dcedcc3ffae58d454d21d10d93556508bf69a
Ryan Van Etten <ryanve@gmail.com>
2013-10-04 17:06:28 -0400

    0.2.3: Use optionalDependencies.

--- GruntFile.js
+++ GruntFile.js
@@ -1,56 +1,47 @@
 module.exports = function(grunt) {
     var _ = require('blood')
+      , pkg = grunt.file.readJSON('package.json')
       , srcDir = 'node_modules/'
-      , tarDir = 'js/'
-      , manifest = {
-            'aok': 'aok',
-            'blood': 'blood',
-            'cargo': 'cargo',
-            'cmon': 'cmon',
-            'dj': 'dj',
-            'dope': 'dope',
-            'elo': 'elo',
+      , tarDir = 'js/' 
+      , manifest = _.keys(pkg.optionalDependencies)
+      , destinations = _.assign(_.object(manifest, manifest), _.pick({
             'ender-js': 'ender',
-            'oi': 'oi',
-            'res': 'res',
-            'response.js': 'response',
-            'scan': 'scan',
-            'underscore': 'underscore',
-            'verge': 'verge',
-            'vibe': 'vibe'
-        };
-        
+            'response.js': 'response'      
+        }, manifest));
+    
     function createFilesMap(suffix) {
-        return _.inject(manifest, function(memo, base, name) {
+        return _.inject(destinations, function(memo, base, name) {
             memo[tarDir + base + '/' + base + suffix] = [srcDir + name + '/' + base + '.js'];
             return memo;
         }, {});
     }
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: pkg,
         concat: {
-            options: {
-                banner: ''
-            },
-            build: {
-                files: createFilesMap('.js')
-            }
+            options: { banner: '' },
+            build: { files: createFilesMap('.js') }
         },
         uglify: {
+            options: { preserveComments: 'some' },
+            build: { files: createFilesMap('.min.js') }
+        },
+        jshint: {
+            dir: ['*.js'], // current dir
+            grunt: ['GruntFile.js'],
             options: {
-                preserveComments: 'some',
-            },
-            build: {
-                files: createFilesMap('.min.js')
+                ignores: ['**/**/node_modules/', '**/**/vendor/', '**/**.min.js'], // in any dir
+                expr:true, sub:true, supernew:true, debug:true, node:true, 
+                boss:true, devel:true, evil:true, laxcomma:true, eqnull:true, 
+                undef:true, unused:true, browser:true, jquery:true, maxerr:10
             }
         }
     });
     
-    // Load task plugins
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // 'default' is run by typing 'grunt' on the command line
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('default', ['jshint:dir', 'concat', 'uglify']);
 };

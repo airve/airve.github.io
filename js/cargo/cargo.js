f290fe62f5e6d7c06778a81afa11492239965706
Ryan Van Etten <ryanve@gmail.com>
2014-01-23 14:54:13 -0500

    Updates: aok@1.7.3 cargo@0.6.2 response.js@0.7.10

--- js/cargo/cargo.js
+++ js/cargo/cargo.js
@@ -1,68 +1,64 @@
 /*!
- * cargo        HTML5 localStorage/sessionStorage module
- * @author      Ryan Van Etten <@ryanve>
- * @link        github.com/ryanve/cargo
- * @license     MIT
- * @version     0.2.0
+ * cargo 0.6.2+201401211835
+ * https://github.com/ryanve/cargo
+ * MIT License 2014 Ryan Van Etten
  */
 
-/*jslint browser: true, devel: true, node: true, passfail: false, bitwise: true, continue: true
-, debug: true, eqeq: true, es5: true, forin: true, newcap: true, nomen: true, plusplus: true
-, regexp: true, undef: true, sloppy: true, stupid: true, sub: true, vars: true, white: true
-, indent: 4, maxerr: 180 */
-
-(function(root, name, definition) {// github.com/umdjs/umd
-    if (typeof module != 'undefined' && module['exports']) { 
-        module['exports'] = definition(); // node|ender|common
-    } else { root[name] = definition(); } // browser
+(function(root, name, make) {
+  if (typeof module != 'undefined' && module['exports']) module['exports'] = make();
+  else root[name] = make();
 }(this, 'cargo', function() {
 
-    // Use array notation on public props to ensure safe compilation
-    // developers.google.com/closure/compiler/docs/api-tutorial3
-    // developers.google.com/closure/compiler/docs/js-for-compiler
+  var cargo = {}
+    , win = typeof window != 'undefined' && window
+    , son = typeof JSON != 'undefined' && JSON || false
+    , has = {}.hasOwnProperty;
     
-    return (function(window) {
-        
-        var localStorage = window['localStorage']
-          , sessionStorage = window['sessionStorage']
-          , testStorage = function(api, key) {
-                try {
-                    key = key || 'cargo';
-                    api['setItem'](key, key);
-                    api['removeItem'](key);
-                    return 1;
-                } catch (e) {}
-            }
-          , JSON = window['JSON'] || false
-          , notWhitespace = /\S+/g
-          , cargo = {};
+  function clone(o) {
+    var k, r = {};
+    for (k in o) has.call(o, k) && (r[k] = o[k]);
+    return r;
+  }
+  
+  function test(api, key) {
+    try {
+      key = key || 'cargo'+-new Date;
+      api['setItem'](key, key);
+      api['removeItem'](key);
+      return true;
+    } catch (e) {}
+    return false;
+  }
+    
+  function abstracts(api) {
+    var und, stores = test(api), cache = {}, all = stores ? api : cache;
+    function f(k, v) {
+      var n = arguments.length;
+      if (1 < n) return und === v ? f['remove'](k) : f['set'](k, v), v;
+      return n ? f['get'](k) : clone(all);
+    }
+    f['stores'] = stores;
+    f['decode'] = son['parse'];
+    f['encode'] = son['stringify'];
+    f['get'] = stores ? function(k) {
+      return und == (k = api['getItem'](k)) ? und : k;
+    } : function(k) {
+      return !has.call(cache, k) ? und : cache[k];
+    };
+    f['set'] = stores ? function(k, v) {
+      api['setItem'](k, v);
+    } : function(k, v) {
+      cache[k] = v;
+    };
+    f['remove'] = stores ? function(k) {
+      api['removeItem'](k);
+    } : function(k) {
+      delete cache[k];
+    };
+    return f;
+  }
 
-        function makeAbstraction(api) {
-            return testStorage(api) ? {
-                'get': function(k) {
-                    k = api['getItem'](k);
-                    return null == k ? void 0 : k;
-                }
-              , 'set': function(k, v) {
-                    v = typeof v == 'function' ? v.call(this, this['get'](k)): v;
-                    api['setItem'](k, v);
-                    return v;
-                }
-              , 'remove': function(k) {
-                    var i = '';
-                    k = (i + k).match(notWhitespace) || i;
-                    i = i.length;
-                    while (i--) {
-                        api['removeItem'](k[i]);
-                    }
-                }
-              , 'decode': JSON['parse']
-              , 'encode': JSON['stringify']
-            } : false;
-        }
-
-        cargo['session'] = makeAbstraction(sessionStorage);
-        cargo['local'] = makeAbstraction(localStorage);
-        return cargo;
-    }(window));
+  cargo['session'] = abstracts(win['sessionStorage']);
+  cargo['local'] = abstracts(win['localStorage']);
+  return cargo;
 }));
